561dbb9576fcb8f0c899624a14c17b1fd46df33d
Arkiver2 <Arkiver@hotmail.com>
2014-09-01 22:29:23 +0200

    tele2.lua: set tries on status code "0" to 40

--- tele2.lua
+++ tele2.lua
@@ -91,7 +91,7 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
 
     tries = tries + 1
 
-    if tries >= 5 then
+    if tries >= 40 then
       io.stdout:write("\nI give up...\n")
       io.stdout:flush()
       return wget.actions.ABORT
